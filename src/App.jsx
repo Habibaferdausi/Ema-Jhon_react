b7c80b3e5fcc90dba9d59be938ce7d3cf78c0b0f
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-03-27 00:00:28 +0600

    add products

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Component/Header/Header";
+import Shop from "./Component/Shop/Shop";
 
 const App = () => {
   return (
     <div className="App">
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 };
