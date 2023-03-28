d2071c73f95af9270e2e2a321105886f1debe704
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-03-28 16:28:17 +0600

    update db

--- src/Component/Shop/Shop.jsx
+++ src/Component/Shop/Shop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { addToDb } from "../../utilities/fakedb";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
@@ -11,6 +11,10 @@ const Shop = () => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
+  }, []);
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+    console.log(storedCart);
   }, []);
 
   const handleAddToCart = (product) => {
