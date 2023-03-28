a61ac11bedda013771bcebb0c01ce94a1354af6b
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-03-28 17:34:07 +0600

    calculation

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -6,9 +6,12 @@ const Cart = (props) => {
   const { cart } = props;
   let total = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    total = total + product.price;
+    product.quantity = product.quantity || 1;
+    total = total + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
   }
 
   const tax = (total * 7) / 100;
@@ -16,7 +19,7 @@ const Cart = (props) => {
   return (
     <div className="cart">
       <h2>Order summary</h2>
-      <p>Selected Items:{cart.length}</p>
+      <p>Selected Items:{quantity}</p>
       <p>Total Price : ${total}</p>
       <p>Total Shipping : ${totalShipping}</p>
       <p>Tax : $ {tax.toFixed(2)} </p>
