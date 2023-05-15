3f5eaaac5d7e2311e8457e6348b1d5123c23a606
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-15 20:13:57 +0600

    update

--- src/Component/Loaders/CardProducts.jsx
+++ src/Component/Loaders/CardProducts.jsx
@@ -1,22 +1,39 @@
 import { getShoppingCart } from "../../utilities/fakedb";
 
 const cartProductsLoader = async () => {
-  const loadedProducts = await fetch("http://localhost:4000/products");
+  // if cart data is in database, you have to use async await
+  const storedCart = getShoppingCart();
+  const ids = Object.keys(storedCart);
+
+  const loadedProducts = await fetch(`http://localhost:4000/productsByIds`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(ids),
+  });
+
   const products = await loadedProducts.json();
 
-  const storedCart = getShoppingCart();
-  const saveCart = [];
+  console.log("products by ID:", products);
+
+  const savedCart = [];
 
   for (const id in storedCart) {
     const addedProduct = products.find((pd) => pd._id === id);
     if (addedProduct) {
       const quantity = storedCart[id];
       addedProduct.quantity = quantity;
-      saveCart.push(addedProduct);
+      savedCart.push(addedProduct);
     }
   }
 
-  return saveCart;
+  // if you need to send two things
+  // return [products, savedCart]
+  // another options
+  // return { products, cart: savedCart }
+
+  return savedCart;
 };
 
 export default cartProductsLoader;
